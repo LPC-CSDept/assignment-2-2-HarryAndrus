81e7e1c6f40a262ed779e6916da626c24af8bcc1
HarryAndrus <iamjoshmay@gmail.com>
2023-02-02 16:18:37 -0800

    ver1

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    cel = float(input("Enter value in Celcius: "))
+    f = (cel * 1.8) + 32
+    print(f)
+    print('%.2f Celcius is equal to = %.2f Degrees Fahrenheit' % (cel, f))
+
 
 
 if __name__ == '__main__':
